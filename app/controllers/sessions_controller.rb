7bf369ac07265593117e97dc0ea68172681993c4
kkrachenfels <kkrachenfels@ucdavis.edu>
2022-04-03 16:37:21 -0700

    Update account balance functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to user_accounts_path(@user)
+      redirect_to user_transactions_path(@user)
     else
       flash[:notice] = "Login is invalid!"
       redirect_to '/login'
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -17,13 +17,13 @@ class TransactionsController < ApplicationController
     @transaction = @user.transactions.build(transaction_params)
 
     if @transaction.save
-      @account = @user.accounts.find(@transaction.account_id)
+      account = @user.accounts.find(@transaction.account_id)
       if @transaction.tType
-        @account[:balance] += @transaction.amount
+        account[:balance] += @transaction.amount
       else
-        @account[:balance] -= @transaction.amount
+        account[:balance] -= @transaction.amount
       end
-      if @account.save
+      if account.save
         redirect_to user_transactions_path(@user)
       else
         render :new, status: :unprocessable_entity
@@ -40,8 +40,33 @@ class TransactionsController < ApplicationController
   def update
     @transaction = @user.transactions.find(params[:id])
 
+    prev_account = @user.accounts.find(@transaction.account_id)
+    prev_amount = @transaction.amount
+    prev_ttype = @transaction.tType
+
     if @transaction.update(transaction_params)
-      redirect_to user_transactions_path(@user)
+      if prev_ttype
+        prev_account[:balance] -= prev_amount
+      else
+        prev_account[:balance] += prev_amount
+      end
+
+      new_account = @user.accounts.find(@transaction.account_id)
+      if new_account.id == prev_account.id
+        new_account[:balance] = prev_account[:balance]
+      end
+
+      if @transaction.tType
+        new_account[:balance] += @transaction.amount
+      else
+        new_account[:balance] -= @transaction.amount
+      end
+
+      if (prev_account.save && new_account.save)
+        redirect_to user_transactions_path(@user)
+      else
+        render :edit, status: :unprocessable_entity
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -49,9 +74,20 @@ class TransactionsController < ApplicationController
 
   def destroy
     @transaction = @user.transactions.find(params[:id])
-    @transaction.destroy
 
-    redirect_to user_transactions_path(@user), status: :see_other
+    account = @user.accounts.find(@transaction.account_id)
+    if @transaction.tType
+      account[:balance] -= @transaction.amount
+    else
+      account[:balance] += @transaction.amount
+    end
+
+    if account.save
+      @transaction.destroy
+      redirect_to user_transactions_path(@user), status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
