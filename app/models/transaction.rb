772ebc582d57bccb27c11a24ca58f5164cc51c4a
kkrachenfels <kkrachenfels@ucdavis.edu>
2022-04-04 13:01:49 -0700

    Adding more efficient functionality to models

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,12 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :account
+
+  def get_type
+    return tType ? "Income" : "Expense"
+  end
+
+  def get_signed_amount
+    return tType ? amount : -amount
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  def get_balance
+    total_balance = 0
+    accounts.each do |account|
+      total_balance += account.balance
+    end
+    return total_balance
+  end
 end
