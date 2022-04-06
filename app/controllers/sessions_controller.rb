e8a2bdb66545c4d5101b7ad317b9c8c2e5f14f76
kkrachenfels <kkrachenfels@ucdavis.edu>
2022-04-05 22:43:19 -0700

    Form styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to user_transactions_path(@user)
+      redirect_to new_user_transaction_path(@user)
     else
       flash[:notice] = "Login is invalid!"
       redirect_to '/login'
