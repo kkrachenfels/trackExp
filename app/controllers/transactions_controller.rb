2508b61f68493babbba138422a8a18a15f8d01a5
kkrachenfels <kkrachenfels@ucdavis.edu>
2022-04-06 13:40:30 -0700

    Minor transaction controller changes

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -18,11 +18,8 @@ class TransactionsController < ApplicationController
 
     if @transaction.save
       account = @user.accounts.find(@transaction.account_id)
-      if @transaction.tType
-        account[:balance] += @transaction.amount
-      else
-        account[:balance] -= @transaction.amount
-      end
+      account[:balance] += @transaction.get_signed_amount
+
       if account.save
         redirect_to user_transactions_path(@user)
       else
@@ -41,26 +38,17 @@ class TransactionsController < ApplicationController
     @transaction = @user.transactions.find(params[:id])
 
     prev_account = @user.accounts.find(@transaction.account_id)
-    prev_amount = @transaction.amount
-    prev_ttype = @transaction.tType
+    prev_amount = @transaction.get_signed_amount
 
     if @transaction.update(transaction_params)
-      if prev_ttype
-        prev_account[:balance] -= prev_amount
-      else
-        prev_account[:balance] += prev_amount
-      end
+      prev_account[:balance] -= prev_amount
 
       new_account = @user.accounts.find(@transaction.account_id)
       if new_account.id == prev_account.id
         new_account[:balance] = prev_account[:balance]
       end
 
-      if @transaction.tType
-        new_account[:balance] += @transaction.amount
-      else
-        new_account[:balance] -= @transaction.amount
-      end
+      new_account[:balance] += @transaction.get_signed_amount
 
       if (prev_account.save && new_account.save)
         redirect_to user_transactions_path(@user)
@@ -76,11 +64,7 @@ class TransactionsController < ApplicationController
     @transaction = @user.transactions.find(params[:id])
 
     account = @user.accounts.find(@transaction.account_id)
-    if @transaction.tType
-      account[:balance] -= @transaction.amount
-    else
-      account[:balance] += @transaction.amount
-    end
+    account[:balance] -= @transaction.get_signed_amount
 
     if account.save
       @transaction.destroy
